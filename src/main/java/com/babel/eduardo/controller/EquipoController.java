8d89890d75a2136155ab6d08d3fa46846c8e08f3
Eduardo Cutillas <eduardo.cutillas@binaryshock.org>
2021-10-21 21:17:39 +0200

    controller

--- src/main/java/com/babel/eduardo/controller/EquipoController.java
+++ src/main/java/com/babel/eduardo/controller/EquipoController.java
@@ -1,8 +1,20 @@
 package com.babel.eduardo.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.babel.eduardo.dto.EquipoDto;
+import com.babel.eduardo.service.impl.EquipoService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -10,4 +22,50 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(path = "/equipo")
 public class EquipoController {
 
+    private final EquipoService equipoService;
+
+    @GetMapping
+    public ResponseEntity<List<EquipoDto>> buscarTodos() {
+	List<EquipoDto> equipo = equipoService.buscarTodos();
+	return ResponseEntity.ok(equipo);
+
+    }
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<EquipoDto> buscar(@RequestParam long id) {
+	EquipoDto equipo = equipoService.buscar(id);
+	if (equipo == null) {
+	    return ResponseEntity.notFound().build();
+	} else {
+	    return ResponseEntity.ok(equipo);
+	}
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> crear(@RequestBody EquipoDto dto) {
+	Long id = equipoService.crear(dto);
+	return ResponseEntity.ok(id);
+    }
+
+    @PutMapping
+    public ResponseEntity<EquipoDto> actualizar(@RequestBody EquipoDto dto) {
+	EquipoDto equipo = equipoService.actualizar(dto);
+	if (equipo == null) {
+	    return ResponseEntity.notFound().build();
+	} else {
+	    return ResponseEntity.accepted().body(equipo);
+	}
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminar(@RequestParam long id) {
+
+	boolean eliminado = equipoService.eliminar(id);
+	if (eliminado) {
+	    return ResponseEntity.noContent().build();
+	} else {
+	    return ResponseEntity.notFound().build();
+	}
+    }
 }
